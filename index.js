842bc327e398c8a3a8a7f594d1ee7d6447a300d1
Denise Kurek <denisekurek@rocketmail.com>
2020-12-04 16:28:48 -0500

    Unpdated button color and margin. Better identified links. Created alerts for Forgot links

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const newAccount = document.querySelector('#newAccount');
 const back = document.querySelector('#back');
 const logForm = document.querySelector('.welcome');
 const signUpForm = document.querySelector('.signup');
+const forgotUser = document.querySelector('#forgotUserName');
+const forgotPass = document.querySelector('#forgotPassword');
 
 
 function log() {
@@ -24,7 +26,18 @@ function goBack() {                   //Hides signup form for login form
   logForm.style.display = "block";
 };
 
+function getUsername() {
+  alert('Forgot Username');
+};
+
+function resetPassword() {
+  alert('Reset Password');
+};
+
+
 login.addEventListener('click', log);
 signup.addEventListener('click', sign);
 newAccount.addEventListener('click', createAccount);
 back.addEventListener('click', goBack);
+forgotUser.addEventListener('click', getUsername);
+forgotPass.addEventListener('click', resetPassword);
