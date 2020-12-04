8df4ba002c9d7c6a2034cc28bca48f0845affc71
[denise] <[denisekurek@rocketmail.com]>
2020-12-04 10:25:11 -0500

    Got the forms to switch on button press. Added the required element to the inputs for both forms.

--- index.js
+++ index.js
@@ -2,24 +2,27 @@ const login = document.querySelector('#login');
 const signup = document.querySelector('#signup');
 const newAccount = document.querySelector('#newAccount');
 const back = document.querySelector('#back');
+const logForm = document.querySelector('.welcome');
+const signUpForm = document.querySelector('.signup');
 
 
 function log() {
   console.log('log me in');
 };
 
-function sign() {
-  console.log('sign me up');
+function sign() {                     //Hides login form for sign up form
+  logForm.style.display = "none";
+  signUpForm.style.display = "block";
 };
 
 function createAccount() {
   console.log('new account');
 };
 
-function goBack() {
-  console.log('Going Back');
+function goBack() {                   //Hides signup form for login form
+  signUpForm.style.display = "none";
+  logForm.style.display = "block";
 };
-
 
 login.addEventListener('click', log);
 signup.addEventListener('click', sign);
